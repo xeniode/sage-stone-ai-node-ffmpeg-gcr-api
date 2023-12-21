875b0481e59e02691718568fe6c870b7c1e49fd9
Garret JH <git@garretjames.com>
2023-12-20 20:10:18 -0600

    init

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# Use the official Node.js 16 image as a parent image
+FROM node:16
+
+# Install FFmpeg
+RUN apt-get update && \
+    apt-get install -y ffmpeg && \
+    apt-get clean
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json (if available)
+COPY package*.json ./
+
+# Install Node.js dependencies
+RUN npm install
+
+# Bundle the app's source code inside the Docker image
+COPY . .
+
+# Your application's default port
+EXPOSE 3000
+
+# Define the command to run the app
+CMD ["node", "app.js"]
---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const ffmpeg = require('fluent-ffmpeg');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON and handle CORS
+app.use(express.json());
+app.use(cors());
+
+// Function to extract audio from video
+function extractAudioFromVideo(videoPath, outputPath) {
+    return new Promise((resolve, reject) => {
+        ffmpeg(videoPath)
+            .output(outputPath)
+            .noVideo()
+            .audioCodec('libmp3lame')
+            .on('end', () => resolve(outputPath))
+            .on('error', (err) => reject(err))
+            .run();
+    });
+}
+
+// POST endpoint to trigger audio extraction
+app.post('/extract-audio', async (req, res) => {
+    try {
+        const { videoPath, outputPath } = req.body;
+        const audioPath = await extractAudioFromVideo(videoPath, outputPath);
+        res.send({ message: 'Audio extracted successfully', audioPath });
+    } catch (error) {
+        res.status(500).send({ message: 'Error extracting audio', error: error.message });
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
